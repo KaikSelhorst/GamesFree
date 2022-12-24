b8af61cf0325405f901d42256ecbc012f44257b6
Kaik Selhorst <contato.kaikselhorst@gmail.com>
2022-12-24 13:48:21 -0400

    Fix: Bug CORS services Fetch

--- src/services.js
+++ src/services.js
@@ -1,34 +1,23 @@
-const baseURL = "https://free-to-play-games-database.p.rapidapi.com/api/games";
-const gameURL =
-  "https://free-to-play-games-database.p.rapidapi.com/api/game?id=";
+import axios from "axios";
 
-const options = {
-  method: "GET",
-  headers: {
-    "X-RapidAPI-Key": "1c76ecaa0amshca2181b2277ed20p1188bejsn1cdb084430a4",
-    "X-RapidAPI-Host": "free-to-play-games-database.p.rapidapi.com",
-  },
-};
+const axiosInstance = axios.create({
+  baseURL:
+    "https://cors-anywhere.herokuapp.com/https://www.freetogame.com/api/games",
+  Origin: "https://www.freetogame.com/api/games",
+});
+const axiosInstanceGame = axios.create({
+  baseURL:
+    "https://cors-anywhere.herokuapp.com/https://www.freetogame.com/api/game",
+  Origin: "https://www.freetogame.com/api/game",
+});
 
 export const api = {
   async get(url) {
-    if (url === "/") url = "";
-    try {
-      const res = await fetch(`${baseURL}${url}`, options);
-      const data = await res.json();
-      return data;
-    } catch (error) {
-      console.log(error);
-    }
+    const { data } = await axiosInstance.get(url);
+    return data;
   },
   async getGame(game) {
-    try {
-      const res = await fetch(`${gameURL}${game}`, options);
-      const data = await res.json();
-
-      return data;
-    } catch (error) {
-      console.log(error);
-    }
+    const { data } = await axiosInstanceGame.get("?id=" + game);
+    return data;
   },
 };
